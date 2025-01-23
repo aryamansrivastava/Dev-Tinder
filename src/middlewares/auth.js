ccfeb2d7ea6001e7f011b61ec31a65c6b53855ce
aryamansrivastava <srivastavaaryaman6@gmail.com>
2025-01-23 11:09:46 +0530

    writing auth middlewares

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,4 @@
-export const adminAuth = (req, res, next) => {
+const adminAuth = (req, res, next) => {
     console.log("Admin Auth is getting checked");
     const token = "xyz";
     const isAdminAuthorized = token === "xyz";
@@ -9,3 +9,20 @@ export const adminAuth = (req, res, next) => {
         next();
     }
 };
+
+const userAuth = (req, res, next) => {
+    console.log("Admin Auth is getting checked");
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if(!isAdminAuthorized){
+        res.status(401).send("UnAuthorized Request");
+    }
+    else{
+        next();
+    }
+};
+
+module.exports = {
+    adminAuth,
+    userAuth,
+}
