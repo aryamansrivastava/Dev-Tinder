e32d90d4bfabb291daefb1eec25460109269f895
aryamansrivastava <srivastavaaryaman6@gmail.com>
2025-01-28 15:11:40 +0530

    Create ConnectionRequestSchema and Implemented indexes in DB

--- src/models/user.js
+++ src/models/user.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minLength: 4,
+        maxLength: 50,
     },  
     lastName: {
         type: String
@@ -39,11 +40,15 @@ const userSchema = new mongoose.Schema({
     }, 
     gender: {
         type: String,
-        validate(value) {
-            if(!["male", "female", "others"].includes(value)) {
-                throw new Error("Gender data is not valid");
-            }
+        enum: {
+            values: ["male", "female", "other"],
+            message: `{VALUE} is not a valid gender type`,
         },
+        // validate(value) {
+        //     if(!["male", "female", "others"].includes(value)) {
+        //         throw new Error("Gender data is not valid");
+        //     }
+        // },
     },
     photoUrl: {
         type: String,
@@ -60,6 +65,8 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
   }
 );
+
+userSchema.index({firstName: 1, lastName: 1});
 
 userSchema.methods.getJWT = async function() {
     const user = this;
